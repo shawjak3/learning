1ed814627e8a643cdbc907629512101af00fc766
Jake Shaw <shawjak3@gmail.com>
2017-01-22 20:12:08 -0700

    Working on eloquent.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Post;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -14,3 +15,42 @@
 // Route::resource('posts', 'PostsController');
 Route::get('/contact', 'PostsController@contact');
 Route::get('/post/{id}', 'PostsController@showPost');
+// Using raw queries
+Route::get('/insert', function() {
+	DB::insert('insert into posts(title, content) values(?, ?)', ['Php with Elixir', 'Elixir is pretty dang cool']);
+});
+Route::get('/read', function() {
+	$results = DB::select('select * from posts where id = ?', [1]);
+	return $results;
+	// foreach ($results as $result) {
+	// 	return $result->title;
+	// }
+});
+Route::get('/update', function() {
+	$updated = DB::update('update posts set title = ?', ['New updated title']);
+	return $updated;
+});
+Route::get('/delete', function() {
+	$deleted = DB::delete('delete from posts where id = ?', [1]);
+	return $deleted;
+});
+
+// Using eloquent
+Route::get('/find', function() {
+	$posts = Post::all();
+	foreach ($posts as $post) {
+		return $post->title;
+	}
+});
+
+Route::get('/findone', function() {
+	$post = Post::find(2);
+	return $post->title;
+});
+
+Route::get('/where', function() {
+	$posts = Post::where('id', 2)->orderBy('id', 'desc')->take(1)->get();
+	return $posts;
+});
+
+
